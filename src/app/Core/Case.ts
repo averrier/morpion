61ddb3c71995d35ddf97a29cd34e05d95e1a3de9
averrier <alex0317@hotmail.fr>
2020-01-21 11:29:19 +0100

    Add zone number to cases

--- src/app/Core/Case.ts
+++ src/app/Core/Case.ts
@@ -1,6 +1,11 @@
 export class Case {
     value = '';
+    zoneNb;
 
+    constructor(myZoneNb) {
+        this.zoneNb = myZoneNb;
+    }
+    
     public setValue(val: string) {
         this.value = val;
     }
